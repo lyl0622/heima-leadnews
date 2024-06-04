2ae17369a8a4e5fed275aa80b9e265c3c120ebc2
“lyl0622” <2623919442@qq.com>
2024-06-04 20:09:18 +0800

    文章列表查询功能实现

---
+++ heima-leadnews-common/src/main/java/com/heima/common/constants/ArticleConstants.java
@@ -0,0 +1,11 @@
+package com.heima.common.constants;
+
+public class ArticleConstants {
+
+    public static final Integer DEFAULT_PAGE_SIZE=10;
+    public static final Integer MAX_PAGE_SIZE=50;
+    public static final Short LOADTYPE_LOAD_MORE = 1;
+    public static final Short LOADTYPE_LOAD_NEW = 2;
+    public static final String DEFAULT_TAG = "__all__";
+}
+
---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ApArticleServiceImpl.java
@@ -0,0 +1,57 @@
+package com.heima.article.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.heima.article.mapper.ApArticleMapper;
+import com.heima.article.service.ApArticleService;
+import com.heima.common.constants.ArticleConstants;
+import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.article.pojos.ApArticle;
+import com.heima.model.common.dtos.ResponseResult;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ApArticleServiceImpl extends ServiceImpl<ApArticleMapper, ApArticle> implements ApArticleService {
+
+    @Autowired
+    private ApArticleMapper apArticleMapper;
+    /**
+     * 加载文章列表
+     *
+     * @param dto
+     * @param type 1 加载更多  2 加载最新
+     * @return
+     */
+    @Override
+    public ResponseResult load(ArticleHomeDto dto, Short type) {
+        //1.校验参数
+        Integer size = dto.getSize();
+        if(size==null||size==0){
+            size= ArticleConstants.DEFAULT_PAGE_SIZE;
+        }
+        size = Math.min(size,ArticleConstants.MAX_PAGE_SIZE);
+        dto.setSize(size);
+
+        //类型参数检验
+        if(!type.equals(ArticleConstants.LOADTYPE_LOAD_MORE) && !type.equals(ArticleConstants.LOADTYPE_LOAD_NEW)){
+            type=ArticleConstants.LOADTYPE_LOAD_MORE;
+        }
+
+        //文章频道检验
+        if(StringUtils.isEmpty(dto.getTag())){
+            dto.setTag(ArticleConstants.DEFAULT_TAG);
+        }
+
+        //时间检验
+        if(dto.getMaxBehotTime()==null)dto.setMaxBehotTime(new Date());
+        if(dto.getMinBehotTime()==null)dto.setMinBehotTime(new Date());
+
+        List<ApArticle> apArticleList = apArticleMapper.loadArticleList(dto, type);
+        return ResponseResult.okResult(apArticleList);
+    }
+}
